496cfc6a52304fb68976f0d8ed9c71633cde9245
Andrew Toups <mistertoups@gmail.com>
2016-10-21 16:32:36 -0400

    Added a buncha animations

--- public/src/js/animations.js
+++ public/src/js/animations.js
@@ -1,34 +1,133 @@
+const displayArea = '#game-container';
+
 var animations = {
+
+  images: ['apple.svg', 'bowling.svg', 'box.svg', 'cabbage.svg', 'cactus.svg', 'can.svg', 'cat.svg', 'cleaver.svg', 'cockroach.svg', 'coffin.svg', 'cookie.svg', 'cup.svg', 'fish.svg', 'floppydisk.svg', 'gyro.svg', 'heart.svg', 'knife.svg', 'pill.svg', 'pillbottle.svg', 'potatoes.svg', 'pumpkin.svg', 'r2d2.svg', 'rainbow.svg', 'scissors.svg', 'shoe.svg', 'skateboard.svg', 'smartphone.svg', 'snowglobe.svg', 'sofa.svg', 'swissarmyknife.svg', 'teabag.svg', 'tentacle.svg', 'triangle.svg', 'umbrella.svg', 'voodoodoll.svg', 'wallet.svg'],
+
+  methods: ['acquire', 'hinge', 'zoomIn'],
+
+  entryStyles: ['bounceIn', 'rubberBand', 'jello', 'flipInX', 'tada'],
+
+  shakeStyles: ['rubberBand', 'jello', 'tada', 'shake', 'wobble'],
+
   getPosition: function(){
     var position = {
-      left: Math.floor(Math.random()* ($('#game-container').outerWidth() - 20)),
-      top: Math.floor(Math.random()* ($('#game-container').outerHeight() - 20)),
+      left: Math.floor(Math.random()* ($(displayArea).outerWidth() - 20)),
+      top: Math.floor(Math.random()* ($(displayArea).outerHeight() - 20)),
     }
+    // position.left = position.left - position.left/2
+    // position.top = position.top - position.top/2
     return position;
   },
 
-  ascend: function(content){
+  getStyle: function(type){
+    var style = animations[type+'Styles'][Math.floor(Math.random()*animations[type+'Styles'].length)];
+    return style;
+  },
+
+  createElem: function(content){
     var position = animations.getPosition();
-    $('<img>')
-    .attr('src', content)
-    .before('+')
+    var elem = $('<img>')
+    .attr({
+      'src': content,
+      'width': '50px',
+    })
     .css({
       'position': 'absolute',
       'top': position.top + 'px',
       'left': position.left + 'px',
     })
-    .prependTo('#game-container')
-    .toggleClass('animated bounceIn')
+    .prependTo(displayArea);
+    return elem;
+  },
+
+  cancel: function(elem){
+    $(elem).removeClass('animated');
+  },
+
+  acquire: function(content){
+    var style = animations.getStyle('entry');
+    var elem = animations.createElem(content);
+    var plus = elem
+      .clone()
+      .attr({
+        'src': 'images/plus.svg',
+        'height': '15px'
+      })
+      .css({
+        'left': '-=35px',
+        'top': '+=10px'
+      })
+      .add(elem);
+    plus.prependTo(displayArea);
+
+    $(plus)
+    .toggleClass('animated ' +style)
     .on('animationend', function(){
-      $(this).toggleClass('linearRise')
-      .on('animationend', function(){
-        $(this).remove();
-      });
+      $(this).toggleClass(style +' fadeOutUpBig')
+        .on('animationend', function(){
+          $(this).remove();
+        });
+    });
+  },
+
+  hinge: function(content){
+    var elem = content instanceof jQuery ? content : animations.createElem(content);
+    $(elem)
+    .toggleClass('animated hinge').
+    on('animationend', function(){
+      $(this).remove();
+    });
+  },
+
+  zoomIn: function(content){
+    var elem = content instanceof jQuery ? content : animations.createElem(content);
+    $(elem)
+    .toggleClass('animated zoomInOut').
+    on('animationend', function(){
+      $(this).remove();
     })
   },
+
+  randomShake: function(elem){
+    var style = animations.getStyle('shake');
+    console.log(style);
+    animations.cancel(elem);
+    $(elem)
+    .toggleClass('animated ' +style)
+    .on('animationend', function(){
+      $(this).removeClass('animated ' +style);
+    });
+  },
+
+  jello: function(){
+
+  }
+
+
+
+
+
+
 };
 
+//DEBUG STUFF
+
 $('#test-button').click(function(){
-  var content = $('#test-content').val();
-  animations.ascend(content);
+  animations.randomShake($(this));
+  var content = $('#test-file').val();
+  var method = $('#test-method').val();
+  animations[method](content);
+});
+
+$('#random-button').click(function(){
+  animations.randomShake($(this));
+  var index = Math.floor(Math.random() * animations.images.length);
+  var method = $('#test-method').val();
+  animations[method]('images/'+ animations.images[index]);
+});
+
+$('#screen-shake').click(function(){
+  animations.randomShake($(this));
+  animations.randomShake(displayArea);
 });
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
 //
+
 console.log('there will be code here someday');
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map
