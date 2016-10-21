8c0139a52bb1fe1727810497b70f8e33f188d0e4
Leigh <leighbryant@bellsouth.net>
2016-10-21 13:39:03 -0400

    Working on creating styles for login page and overall look

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
 //
+
 console.log('there will be code here someday');
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map
