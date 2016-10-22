a27c8ae1cbcc395da01f44792fc57164e81b50aa
Andrew Toups <mistertoups@gmail.com>
2016-10-22 16:28:35 -0400

    Basic format complete, addressing vertical alignment of list items

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -15,3 +15,11 @@ console.log('there will be code here someday');
 //# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
