c47ce9603ef1999a98615ba87170d239838f861f
Andrew Toups <mistertoups@gmail.com>
2016-10-23 18:44:12 -0400

    Added badge constructor and some display methods

--- public/src/js/game.js
+++ public/src/js/game.js
@@ -5,7 +5,92 @@ var addListeners = {
       animations.static($(this), 'rubberBand');
     });
   }
-}
+};
+
+function Badge(badgeObj){
+  this.name = badgeObj.badge;
+  this.value = Number(badgeObj.pointValue);
+  this.image = badgeObj.image;
+  this.text = badgeObj.description;
+  this.init();
+};
+
+Badge.prototype = {
+  init: function(){
+    // call animation
+    animations.acquire(Game.path + this.image);
+    // print description
+    this.showText(this.item);
+    // show point value on screen
+
+
+  },
+
+  showText: function(){
+    $('#message').find('p').text(this.text);
+  },
+};
+
+var Game = {
+  path: '../images/badges/',
+
+  currentBadges: {},
+
+  currentScore: 0,
+
+  grabBadge: function(tier){
+    tier = tier || 'tierOne';
+    var thisTier = (Game.badges[tier]);
+    var index = Math.floor(Math.random()*thisTier.length);
+    var badgeObj = thisTier[index];
+    var badge = new Badge(badgeObj);
+    if (!Game.currentBadges[badge.name]){
+      Game.currentBadges[badge.name] = 1;
+      Game.updateInventory(badge);
+    } else {
+      Game.currentBadges[badge.name]++;
+      Game.updateInventory(badge);
+    }
+    Game.updateScore(badge);
+  },
+
+  updateScore(badge){
+    Game.currentScore += badge.value;
+    $('#score').text(Game.currentScore);
+    // animations.acquire(badge.value.toString(), {'top': '40px', 'right': '40px'});
+  },
+
+  updateInventory(badge){
+    if (Game.currentBadges[badge.name] === 1){
+      $('.' + badge.name)
+        .find('img')
+        .remove();
+      var img = $('<img>')
+        .attr('src', Game.path + badge.image)
+        .css({
+          'opacity': '1'
+        });
+      animations.static($(img).appendTo('.' + badge.name), 'bounceIn');
+    } else {
+      Game.updateCount(badge);
+    }
+  },
+
+  updateCount(badge){
+    if (Game.currentBadges[badge.name] === 2){
+      var badgeCount = $('<div>')
+        .attr('class', 'badgeCount')
+        .text(Game.currentBadges[badge.name]);
+      animations.static($(badgeCount).appendTo('.' + badge.name), 'bounceIn');
+    } else {
+      $('.' + badge.name)
+        .find('.badgeCount')
+        .text(Game.currentBadges[badge.name]);
+    }
+  }
+};
+
+
 
 window.onload = function(){
   console.log('loaded');
