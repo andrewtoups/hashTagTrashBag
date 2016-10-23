ab9ad9230c52a56514fdb781916c80dd1f64a1bf
Andrew Toups <mistertoups@gmail.com>
2016-10-23 12:58:55 -0400

    Added testing code when window loads

--- public/src/js/game.js
+++ public/src/js/game.js
@@ -7,5 +7,18 @@ var addListeners = {
   }
 }
 
-addListeners.buttons('#wrong');
-addListeners.buttons('#right');
+window.onload = function(){
+  console.log('loaded');
+  $('<img>')
+    .attr('src', '../images/numerals/' +utils.getNumeral(Math.floor(Math.random()*9)))
+    .appendTo('#L');
+  $('<img>')
+    .attr('src', '../images/numerals/' +utils.getNumeral(Math.floor(Math.random()*9)))
+    .appendTo('#R');
+  $('<img>')
+    .attr('src', '../images/operators/' +utils.getRandomOperator())
+    .appendTo('#operator');
+
+  addListeners.buttons('#wrong');
+  addListeners.buttons('#right');
+};
