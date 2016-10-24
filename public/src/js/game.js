a7ebbfb58cf6e8afaf96d748f0fc78ab6fb5d68e
Andrew Toups <mistertoups@gmail.com>
2016-10-24 00:04:19 -0400

    Updated event handlers for continuous play

--- public/src/js/game.js
+++ public/src/js/game.js
@@ -1,10 +1,24 @@
 var addListeners = {
-  buttons: function(elem){
-    $(elem).mousedown(function(){
-      console.log('clickdown');
-      animations.static($(this), 'rubberBand');
+  right: function(elem){
+    $(elem)
+    .mousedown(function(){
+      animations.static(this, 'rubberBand');
+    })
+    .mouseup(function(){
+      Game.check(true);
+    });
+  },
+
+  wrong: function(elem){
+    $(elem)
+    .mousedown(function(){
+      animations.static(this, 'rubberBand');
+    })
+    .mouseup(function(){
+      Game.check(false);
     });
   }
+
 };
 
 function Badge(badgeObj){
@@ -94,16 +108,5 @@ var Game = {
 
 window.onload = function(){
   console.log('loaded');
-  $('<img>')
-    .attr('src', '../images/numerals/' +utils.getNumeral(Math.floor(Math.random()*9)))
-    .appendTo('#L');
-  $('<img>')
-    .attr('src', '../images/numerals/' +utils.getNumeral(Math.floor(Math.random()*9)))
-    .appendTo('#R');
-  $('<img>')
-    .attr('src', '../images/operators/' +utils.getRandomOperator())
-    .appendTo('#operator');
-
-  addListeners.buttons('#wrong');
-  addListeners.buttons('#right');
+  Game.init();
 };
